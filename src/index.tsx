5cd6f3b92a7cc31d77dc2ebc8e219a0d97499ef2
HarmonySkull <developer@harmonyskull.codes>
2022-01-17 17:25:30 -0500

    CREATE: ReactJS base project;

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+import './styles/base/_base.css'
+
+ReactDOM.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+	document.getElementById('root')
+)
