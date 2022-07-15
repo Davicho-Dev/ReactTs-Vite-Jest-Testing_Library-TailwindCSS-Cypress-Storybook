89d220e738c4b1519f918616ecce6b6f757c1c7d
HarmonySkull <developer@harmonyskull.codes>
2022-07-15 09:50:53 -0500

    CREATE: folder structure;
    Signed-off-by: HarmonySkull <developer@harmonyskull.codes>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import { StrictMode } from 'react'
+
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+import './styles/globals.sass'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+
+  </StrictMode>
 )
