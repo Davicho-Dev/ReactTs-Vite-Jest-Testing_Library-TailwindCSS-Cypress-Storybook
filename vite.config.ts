f9fc76713a6b2e15f2816410d43c64777d3fff02
HarmonySkull <developer@harmonyskull.codes>
2022-07-16 03:30:39 -0500

    UPDATE: localhost default port;
    Signed-off-by: HarmonySkull <developer@harmonyskull.codes>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+	plugins: [react()],
+	server: {
+		port: 3000,
+	},
+	preview: {
+		port: 8080,
+	},
 })
